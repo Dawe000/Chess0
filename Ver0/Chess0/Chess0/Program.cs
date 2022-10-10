8b1f814ed65308d91239d630f1cb6c4102185740
dawid <dawid@LAPTOP-SSMU1FO5>
2022-10-10 10:06:34 +0100

    Add project files.

---
+++ Ver0/Chess0/Chess0/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Chess0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+
+    public class Chess
+    {
+        char[,] board;
+        string[] players;
+        int[] points;
+        int[] time;
+        int increment;
+
+        public Chess(string[] iPlayers, int iTime, int iIncrement)
+        {
+            board = new char[,]{ 
+                    { 'R','N','B','K','Q','B','N','R' },
+                    { 'P','P','P','P','P','P','P','P' },
+                    { ' ',' ',' ',' ',' ',' ',' ',' ' },
+                    { ' ',' ',' ',' ',' ',' ',' ',' ' },
+                    { ' ',' ',' ',' ',' ',' ',' ',' ' },
+                    { ' ',' ',' ',' ',' ',' ',' ',' ' },
+                    { 'P','P','P','P','P','P','P','P' },
+                    { 'R','N','B','K','Q','B','N','R' }};
+            players = iPlayers;
+            points = new int[] { 0, 0 };
+            time = new int[] { iTime, iTime };
+            increment = iIncrement;
+
+
+        }
+
+    }
+        
+}
