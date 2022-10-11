e9644cba87c7874c8b2b77ae18923f39c531319c
Dawid Pisarczyk <dawid.pisarczyk@gmail.com>
2022-10-11 11:25:02 +0100

    Adding to PGN file

--- Ver0/Chess0/Chess0/PGN.cs
+++ Ver0/Chess0/Chess0/PGN.cs
@@ -4,10 +4,19 @@ using System.Text;
 
 namespace Chess0
 {
-    class PGN
+    public class PGN
     {
-        public string Event { get; private set; }
-        public string Site { get; private set; }
+        private string Event { get; set; }
+        private string Site { get; set; }
+        private DateTime Date { get; set; }
+        private int Round { get; set; }
+        private string White { get; set; }
+        private string Black { get; set; }
+        private string Result { get; set; }
 
+        public PGN(string White, string Black, string Event = "Chess0 Game", string Site = "Chess0", DateTime Date = DateTime.Now, )
+        {
+
+        }
     }
 }
