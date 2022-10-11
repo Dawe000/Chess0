6063c74a57648dd10fc7ff20163a49848a636e85
Dawid Pisarczyk <dawid.pisarczyk@gmail.com>
2022-10-11 10:58:26 +0100

    Im trying to commit idk how!!

---
+++ Ver0/Chess0/Chess0/Chess.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Chess0
+{
+    public class Chess
+    {
+        {
+        string[,] board;
+        string[] players;
+        int[] points;
+        int[] time;
+        int increment;
+        int turn;
+
+        public Chess(string[] iPlayers, int iTime, int iIncrement)
+        {
+            board = new string[,]{
+                    { "0R","0N","0B","0K","0Q","0B","0N","0R" },
+                    { "0P","0P","0P","0P","0P","0P","0P","0P" },
+                    { "","","","","","","","" },
+                    { "","","","","","","","" },
+                    { "","","","","","","","" },
+                    { "","","","","","","","" },
+                    { "1P","1P","1P","1P","1P","1P","1P","1P" },
+                    { "1R","1N","1B","1K","1Q","1B","1N","1R" }};
+            players = iPlayers;
+            points = new int[] { 0, 0 };
+            time = new int[] { iTime, iTime };
+            increment = iIncrement;
+            turn = 0;
+        }
+
+        public string[] CheckLegalMoves(int[] pos) //returns all legal moves for a piece
+        {
+            char type = board[pos[0], pos[1]][1];
+        }
+
+    }
+}
+
--- Ver0/Chess0/Chess0/Program.cs
+++ Ver0/Chess0/Chess0/Program.cs
@@ -6,37 +6,8 @@ namespace Chess0
     {
         static void Main(string[] args)
         {
-            
+
         }
     }
 
-    public class Chess
-    {
-        char[,] board;
-        string[] players;
-        int[] points;
-        int[] time;
-        int increment;
-
-        public Chess(string[] iPlayers, int iTime, int iIncrement)
-        {
-            board = new char[,]{ 
-                    { 'R','N','B','K','Q','B','N','R' },
-                    { 'P','P','P','P','P','P','P','P' },
-                    { ' ',' ',' ',' ',' ',' ',' ',' ' },
-                    { ' ',' ',' ',' ',' ',' ',' ',' ' },
-                    { ' ',' ',' ',' ',' ',' ',' ',' ' },
-                    { ' ',' ',' ',' ',' ',' ',' ',' ' },
-                    { 'P','P','P','P','P','P','P','P' },
-                    { 'R','N','B','K','Q','B','N','R' }};
-            players = iPlayers;
-            points = new int[] { 0, 0 };
-            time = new int[] { iTime, iTime };
-            increment = iIncrement;
-
-
-        }
-
-    }
-        
 }
