746f1853c17ca8867c772d5dc58ea1e939edb2b7
Dawe000 <dawid.pisarczyk@gmail.com>
2022-10-11 18:31:48 +0100

    Initial setup for PGN class

--- Ver0/Chess0/Chess0/PGN.cs
+++ Ver0/Chess0/Chess0/PGN.cs
@@ -13,12 +13,21 @@ namespace Chess0
         private string White { get; set; }
         private string Black { get; set; }
         private string Result { get; set; }
+        private List<string> MoveText { get; set; }
 
-        public PGN(string iWhite, string iBlack, string iEvent = "Chess0 Game", string iSite = "Chess0", DateTime iDate = default, int iRound = 1, string iResult = "*")
+        public PGN(string iWhite, string iBlack, string iEvent = "Chess0 Game", string iSite = "Chess0", DateTime iDate = default, int iRound = 1, string iResult = "*", List<string> iMoveText = default)
         {
             Event = iEvent;
             Site = iSite;
-
+            Date = DateTime.Now;
+            if (!(iDate == default)) Date = iDate;
+            Round = iRound;
+            White = iWhite;
+            Black = iBlack;
+            Result = iResult;
+            MoveText = iMoveText;
         }
+
+        
     }
 }
