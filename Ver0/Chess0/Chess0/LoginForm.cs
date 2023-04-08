50b59b07e14c2dd8c3ada033d77eafbad62f107f
Dawe000 <dawid.pisarczyk@gmail.com>
2023-04-08 21:17:32 +0100

    Fully commented LoginForm.cs

--- Ver0/Chess0/Chess0/LoginForm.cs
+++ Ver0/Chess0/Chess0/LoginForm.cs
@@ -13,13 +13,14 @@ namespace Chess0
 {
     public partial class LoginForm : Form
     {
+        
         string player;
         string[] usedNames;
         List<string> userNames;
         List<string> hashes;
         StartForm main;
 
-        public LoginForm(string iplayer,string ip1, string ip2,StartForm s)
+        public LoginForm(string iplayer,string ip1, string ip2,StartForm s) //constructor, finds all the usernames and hashes ready for logging in and registering new users
         {
             InitializeComponent();
             main = s;
@@ -29,25 +30,25 @@ namespace Chess0
             hashes = System.IO.File.ReadAllLines(System.IO.Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location) + @"\RequiredFiles\Hashes.txt").ToList() ;
         }
 
-        private void LoginButton_Click(object sender, EventArgs e)
+        private void LoginButton_Click(object sender, EventArgs e) //runs when the login button is clicked
         {
-            if (!userNames.Contains(UsernameBox.Text))
+            if (!userNames.Contains(UsernameBox.Text)) //checks if the account exists
             {
                 InfoLBL.Text = "No account with this username exists";
                 return;
             }
-            if (usedNames.Contains(UsernameBox.Text))
+            if (usedNames.Contains(UsernameBox.Text)) //checks if the account is already logged in
             {
                 InfoLBL.Text = "Already logged in";
                 return;
             }
             int i = 0;
-            while (userNames[i] != UsernameBox.Text)
+            while (userNames[i] != UsernameBox.Text) //finds index of username in username list
             {
                 i++;
             }
-            Crypt c = new Crypt(PasswordBox.Text);
-            if (c.MD5HashGen(PasswordBox.Text + "EXTRA") == hashes[i])
+            Crypt c = new Crypt(PasswordBox.Text); 
+            if (c.MD5HashGen(PasswordBox.Text + "EXTRA") == hashes[i]) //check if password entered matches the corresponding hash
             {
                 login();
             }
@@ -62,7 +63,7 @@ namespace Chess0
         {
             string path = System.IO.Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location) + @"\RequiredFiles\" + UsernameBox.Text;
             Image profileImage = default;
-            if (File.Exists(path + @"\ProfilePicture.png"))
+            if (File.Exists(path + @"\ProfilePicture.png")) //finds the users profile picture if it exists
             {
                 using (FileStream stream = new FileStream(path + @"\ProfilePicture.png", FileMode.Open, FileAccess.Read))
                 {
@@ -70,22 +71,22 @@ namespace Chess0
                 }
             }
             Crypt c = new Crypt(PasswordBox.Text);
-            main.Login(UsernameBox.Text, PasswordBox.Text, player,profileImage);
+            main.Login(UsernameBox.Text, PasswordBox.Text, player,profileImage); //send account information to menu form
             this.Close();
             main.Enabled = true;
         }
-        private void RegisterButton_Click(object sender, EventArgs e)
+        private void RegisterButton_Click(object sender, EventArgs e) //run when the register button is clicked
         {
-            if (UsernameBox.TextLength>6 || UsernameBox.TextLength < 1)
+            if (UsernameBox.TextLength>6 || UsernameBox.TextLength < 1) //checks username length
             {
                 InfoLBL.Text = "Invalid username length";
                 return;
             }
-            if (userNames.Contains(UsernameBox.Text)){
+            if (userNames.Contains(UsernameBox.Text)){ //checks if username is taken
                 InfoLBL.Text = "Username taken";
                 return;
             }
-            if (UsernameBox.Text == "Guest1" || UsernameBox.Text == "Guest2")
+            if (UsernameBox.Text == "Guest1" || UsernameBox.Text == "Guest2") 
             {
                 InfoLBL.Text = "Invalid username";
                 return;
@@ -101,7 +102,7 @@ namespace Chess0
                 return;
             }
             
-            string NewDirectory = System.IO.Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location) + @"\RequiredFiles\" + UsernameBox.Text;
+            string NewDirectory = System.IO.Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location) + @"\RequiredFiles\" + UsernameBox.Text; //creates a new directory for the new user
             System.IO.Directory.CreateDirectory(NewDirectory);
             using (StreamWriter sw = File.CreateText(NewDirectory + @"\Data.txt"))
             {
@@ -109,18 +110,18 @@ namespace Chess0
             }
             Crypt c = new Crypt(PasswordBox.Text);
             userNames.Add(UsernameBox.Text);
-            System.IO.File.WriteAllLines(System.IO.Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location) + @"\RequiredFiles\Users.txt", userNames);
+            System.IO.File.WriteAllLines(System.IO.Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location) + @"\RequiredFiles\Users.txt", userNames); //adds the new users username to the username list
             hashes.Add(c.MD5HashGen(PasswordBox.Text + "EXTRA"));
-            System.IO.File.WriteAllLines(System.IO.Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location) + @"\RequiredFiles\Hashes.txt", hashes);
+            System.IO.File.WriteAllLines(System.IO.Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location) + @"\RequiredFiles\Hashes.txt", hashes); //adds the new users password hash to the hashes
             string data = "0^0^0^0";
             string cypherText = c.FullEncrypt(data);
-            System.IO.File.WriteAllText(System.IO.Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location) + @"\RequiredFiles\"+UsernameBox.Text+@"\Data.txt", cypherText);
+            System.IO.File.WriteAllText(System.IO.Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location) + @"\RequiredFiles\"+UsernameBox.Text+@"\Data.txt", cypherText); //encrypts the new users data and stores it in their directory
             login();
         }
 
         private void LoginForm_FormClosed(object sender, FormClosedEventArgs e)
         {
-            main.Enabled = true;
+            main.Enabled = true; 
         }
     }
 }
